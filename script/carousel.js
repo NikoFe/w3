7bc493eb9d94ce3305bfaf1c69bf0c4ea7cdb022
Niko <niko.ferk@stdudent.um.si>
2024-07-29 11:02:13 +0200

    finished design for home page

---
+++ script/carousel.js
@@ -0,0 +1,8 @@
+var elem = document.querySelector(".main-carousel");
+var flkty = new Flickity(elem, {
+  cellAlign: "left",
+  contain: false,
+  // autoPlay: 1500,
+  pauseAutoPlayOnHover: true,
+  resize: true,
+});
