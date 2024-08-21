274d4695037bcc9c43b6d2dc2dd22b3008d0cc89
Niko <niko.ferk@stdudent.um.si>
2024-08-21 22:24:32 +0200

    right before complete restructure of seacrh.js specifically creating a filtered list

--- script/index.js
+++ script/index.js
@@ -1,12 +1,132 @@
 let drops = document.getElementsByClassName("drop");
 let downs = document.getElementsByClassName("down");
 let h3 = document.querySelectorAll("#row-3 h3");
+let firstTime = true;
+
+let znamke = document.querySelectorAll("#znamka-dropdown li");
+let models = document.querySelectorAll("#models-dropdown li");
+znamke[0].style.display = "flex";
 
 const dropdownsP = document.querySelectorAll(".dropdowns p");
 
-console.log("AS LENGTH: " + dropdownsP.length);
-for (let i = 0; i < dropdownsP.length; i++) {
-  // console.log("as: " + dropdownsP[i].innerHTML);
+let selected = document.querySelectorAll("#znamka-dropdown p");
+
+function filterModel() {
+  for (let i = 0; i < znamke.length; i++) {
+    for (let j = 1; j < models.length; j++) {
+      models[j].style.display = "none";
+    }
+
+    console.log("CURRENT ZNAMKA: " + selected[0].innerHTML.toString());
+
+    switch (selected[0].innerHTML.toString()) {
+      case "Volkswagen":
+        console.log("--Volkswagen--");
+        models[1].style.display = "flex";
+
+        break;
+      case "Škoda":
+        console.log("--Škoda--");
+        models[2].style.display = "flex";
+
+        break;
+      case "Hyundai":
+        console.log("--Hyundai--");
+        models[3].style.display = "flex";
+        models[4].style.display = "flex";
+        models[5].style.display = "flex";
+        break;
+      case "Opel":
+        console.log("--Opel--");
+        models[6].style.display = "flex";
+
+        break;
+      case "Citroen":
+        console.log("--Citroen--");
+        models[7].style.display = "flex";
+        models[8].style.display = "flex";
+        break;
+      case "Audi":
+        console.log("--Audi--");
+        models[9].style.display = "flex";
+        models[10].style.display = "flex";
+        break;
+      case "BMW":
+        console.log("--BMW--");
+        models[11].style.display = "flex";
+        models[12].style.display = "flex";
+        break;
+      case "Renault":
+        console.log("--Renault--");
+        models[13].style.display = "flex";
+
+        break;
+      case "Mazda":
+        console.log("--Mazda--");
+        models[14].style.display = "flex";
+        models[15].style.display = "flex";
+        models[16].style.display = "flex";
+        break;
+    }
+  }
+}
+
+for (let i = 0; i < znamke.length; i++) {
+  znamke[i].addEventListener("click", function () {
+    for (let j = 1; j < models.length; j++) {
+      models[j].style.display = "none";
+    }
+
+    switch (znamke[i].innerHTML.toString()) {
+      case "Volkswagen":
+        console.log("--Volkswagen--");
+        models[1].style.display = "flex";
+
+        break;
+      case "Škoda":
+        console.log("--Škoda--");
+        models[2].style.display = "flex";
+
+        break;
+      case "Hyundai":
+        console.log("--Hyundai--");
+        models[3].style.display = "flex";
+        models[4].style.display = "flex";
+        models[5].style.display = "flex";
+        break;
+      case "Opel":
+        console.log("--Opel--");
+        models[6].style.display = "flex";
+
+        break;
+      case "Citroen":
+        console.log("--Citroen--");
+        models[7].style.display = "flex";
+        models[8].style.display = "flex";
+        break;
+      case "Audi":
+        console.log("--Audi--");
+        models[9].style.display = "flex";
+        models[10].style.display = "flex";
+        break;
+      case "BMW":
+        console.log("--BMW--");
+        models[11].style.display = "flex";
+        models[12].style.display = "flex";
+        break;
+      case "Renault":
+        console.log("--Renault--");
+        models[13].style.display = "flex";
+
+        break;
+      case "Mazda":
+        console.log("--Mazda--");
+        models[14].style.display = "flex";
+        models[15].style.display = "flex";
+        models[16].style.display = "flex";
+        break;
+    }
+  });
 }
 
 function createLiList(parent) {
@@ -45,11 +165,11 @@ function storeVariable(data, options) {
 }
 
 function start() {
-  let znamkaOptions = createLiList("znamka-dropdown");
+  let znamkaOptions = createLiList("znamka-dropdown-class");
   let cenaOdOptions = createLiList("cena-od-dropdown");
   let cenaDoOptions = createLiList("cena-do-dropdown");
   let prevozenihOptions = createLiList("prevozenih-dropdown");
-  let modelOptions = createLiList("model-dropdown");
+  let modelOptions = createLiList("model-dropdown-class");
   let letnikOdOptions = createLiList("letnik-od-dropdown");
   let letnikDoOptions = createLiList("letnik-do-dropdown");
   let gorivoOptions = createLiList("gorivo-dropdown");
@@ -147,26 +267,47 @@ function resetFilter() {
   localStorage.setItem("letnik-do", "");
 }
 
-function showLocals() {
-  console.log("ZNAMKA: " + localStorage.getItem("znamka"));
-  console.log("CENA OD: " + localStorage.getItem("cena-od"));
-  console.log("CENA DO: " + localStorage.getItem("cena-do"));
-  console.log("PREVOŽENIH: " + localStorage.getItem("prevozenih"));
+function showLocals() {}
 
-  console.log("GORIVO: " + localStorage.getItem("gorivo"));
-  console.log("PREVOZENIH: " + localStorage.getItem("prevozenih"));
+function fillDrops() {
+  console.log("1: " + drops[0].querySelectorAll("p")[0].innerHTML);
+  console.log("2: " + drops[1].querySelectorAll("p")[0].innerHTML);
+  console.log("3: " + drops[2].querySelectorAll("p")[0].innerHTML);
+  console.log("4: " + drops[3].querySelectorAll("p")[0].innerHTML);
+  console.log("5: " + drops[4].querySelectorAll("p")[0].innerHTML);
 
-  console.log("LETNIK OD: " + localStorage.getItem("letnik-od"));
-  console.log("LETNIK DO: " + localStorage.getItem("letnik-do"));
+  drops[0].querySelectorAll("p")[0].innerHTML = localStorage.getItem("znamka");
+
+  drops[1].querySelectorAll("p")[0].innerHTML = localStorage.getItem("cena-od");
+
+  drops[2].querySelectorAll("p")[0].innerHTML = localStorage.getItem("cena-do");
+
+  drops[3].querySelectorAll("p")[0].innerHTML =
+    localStorage.getItem("prevozenih");
+
+  drops[4].querySelectorAll("p")[0].innerHTML = localStorage.getItem("model");
+  drops[5].querySelectorAll("p")[0].innerHTML =
+    localStorage.getItem("letnik-od");
+  drops[6].querySelectorAll("p")[0].innerHTML =
+    localStorage.getItem("letnik-do");
+  drops[7].querySelectorAll("p")[0].innerHTML = localStorage.getItem("gorivo");
 }
 
 window.onload = function () {
   showLocals();
   start();
+  //resetFilter();
+  /*if (firstTime) {
+    alert("RESET");
+    resetFilter();
+    firstTime = false;
+  }*/
+  fillDrops();
+  filterModel();
 };
 
 function error() {
-  alert(h3[0].innerHTML);
+  //alert(h3[0].innerHTML);
   h3[0].style.color = "red";
   h3[0].innerHTML = "Pozabili ste izbrati iskalne kriterije:";
 }
